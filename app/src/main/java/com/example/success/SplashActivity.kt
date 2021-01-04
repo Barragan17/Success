864a8d2c8e9a617954f656c742f2c5a610aa3f99
Barragan17 <tegartrahman@gmail.com>
2021-01-04 18:03:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/success/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.success
+
+import android.content.Intent
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.HandlerThread
+import android.os.Handler
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
+        tv_title.typeface = typeFace
+
+
+        Handler().postDelayed({
+
+        startActivity(Intent(this, IntroActivity::class.java))
+        finish()
+        }, 2500)
+    }
+
+}
